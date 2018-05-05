1aa32e13ba80f73d60cd9d251770758818eab463
hwchiu <hwchiu@linkernetworks.com>
2018-05-05 15:38:22 +0800

    Refine the dockerfile and support the g option to generate new ovs config

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
-FROM alpine:latest
+FROM golang:1.10-alpine3.7
+MAINTAINER Hung-Wei Chiu <hwchiu@linkernetworks.com>
 
-ADD multus-cni/bin/multus /tmp
+WORKDIR /go/src/github.com/hwchiu/docker-multus-cni
+COPY ./  /go/src/github.com/hwchiu/docker-multus-cni
+
+RUN apk add --no-cache git bzr bash
+RUN go get github.com/kardianos/govendor
+RUN govendor sync
+RUN git submodule init && git submodule update
+WORKDIR multus-cni
+RUN ./build
+
+WORKDIR /go/src/github.com/hwchiu/docker-multus-cni
+RUN go install .
+ADD multus-cni/bin/multus /go/bin/multus
 ADD conf/ /tmp
 ADD yaml/ /tmp
 ADD entrypoint.sh /
+
+ENV DEST_CNI /etc/cni/net.d/00-multus.con
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-while getopts b:c:k: option
+while getopts b:c:k:g: option
 do
     case "${option}"
         in
         b)  echo "Copy CNI Binary to "${OPTARG}
-            cp -f /tmp/multus ${OPTARG}
+            cp -f /go/bin/multus ${OPTARG}
             ;;
         c)
             echo "Copty CNI Conf to "${OPTARG}
@@ -15,5 +15,9 @@ do
             echo "Copty CRD resource to "${OPTARG}
             cp -f /tmp/crdnetwork.yaml ${OPTARG}
             ;;
+        g)
+            echo "Try to embed the CNI ${OPTARG} to mulsu CNI and save in ${DEST_CNI}"
+            echo "/go/bin/docker-multus-cni -input "${OPTARG}" -output "${DEST_CNI}""
+            /go/bin/docker-multus-cni -input "${OPTARG}" -output "${DEST_CNI}"
     esac
 done
